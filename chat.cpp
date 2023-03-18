d656e626502dc4028e3aa3baf12cbda3a25350bd
salmon-coder <salmoncrusher@gmail.com>
2023-03-18 16:07:52 -0500

    reformat

--- chat.cpp
+++ chat.cpp
@@ -173,16 +173,24 @@ bool llama_model_load(const std::string & fname, llama_model & model, gpt_vocab 
     // in order to save memory and also to speed up the computation
     ggml_type wtype = GGML_TYPE_COUNT;
     switch (model.hparams.f16) {
-        case 0: wtype = GGML_TYPE_F32;  break;
-        case 1: wtype = GGML_TYPE_F16;  break;
-        case 2: wtype = GGML_TYPE_Q4_0; break;
-        case 3: wtype = GGML_TYPE_Q4_1; break;
-        default:
-                {
-                    fprintf(stderr, "%s: invalid model file '%s' (bad f16 value %d)\n",
-                            __func__, fname.c_str(), model.hparams.f16);
-                    return false;
-                }
+    case 0:
+        wtype = GGML_TYPE_F32;
+        break;
+    case 1:
+        wtype = GGML_TYPE_F16;
+        break;
+    case 2:
+        wtype = GGML_TYPE_Q4_0;
+        break;
+    case 3:
+        wtype = GGML_TYPE_Q4_1;
+        break;
+    default:
+    {
+        fprintf(stderr, "%s: invalid model file '%s' (bad f16 value %d)\n",
+                __func__, fname.c_str(), model.hparams.f16);
+        return false;
+    }
     }
 
     const ggml_type wtype2 = GGML_TYPE_F32;
@@ -445,15 +453,25 @@ bool llama_model_load(const std::string & fname, llama_model & model, gpt_vocab 
                 size_t bpe = 0;
 
                 switch (ftype) {
-                    case 0: bpe = ggml_type_size(GGML_TYPE_F32);  break;
-                    case 1: bpe = ggml_type_size(GGML_TYPE_F16);  break;
-                    case 2: bpe = ggml_type_size(GGML_TYPE_Q4_0); assert(ne[0] % 64 == 0); break;
-                    case 3: bpe = ggml_type_size(GGML_TYPE_Q4_1); assert(ne[0] % 64 == 0); break;
-                    default:
-                            {
-                                fprintf(stderr, "%s: unknown ftype %d in model file\n", __func__, ftype);
-                                return false;
-                            }
+                case 0:
+                    bpe = ggml_type_size(GGML_TYPE_F32);
+                    break;
+                case 1:
+                    bpe = ggml_type_size(GGML_TYPE_F16);
+                    break;
+                case 2:
+                    bpe = ggml_type_size(GGML_TYPE_Q4_0);
+                    assert(ne[0] % 64 == 0);
+                    break;
+                case 3:
+                    bpe = ggml_type_size(GGML_TYPE_Q4_1);
+                    assert(ne[0] % 64 == 0);
+                    break;
+                default:
+                {
+                    fprintf(stderr, "%s: unknown ftype %d in model file\n", __func__, ftype);
+                    return false;
+                }
                 };
 
                 if (n_dims == 1 || n_parts == 1) {
@@ -531,12 +549,12 @@ bool llama_model_load(const std::string & fname, llama_model & model, gpt_vocab 
 // The GPT-J model requires about 16MB of memory per input token.
 //
 bool llama_eval(
-        const llama_model & model,
-        const int n_threads,
-        const int n_past,
-        const std::vector<gpt_vocab::id> & embd_inp,
-              std::vector<float>         & embd_w,
-              size_t                     & mem_per_token) {
+    const llama_model & model,
+    const int n_threads,
+    const int n_past,
+    const std::vector<gpt_vocab::id> & embd_inp,
+    std::vector<float>         & embd_w,
+    size_t                     & mem_per_token) {
     const int N = embd_inp.size();
 
     const auto & hparams = model.hparams;
@@ -550,7 +568,7 @@ bool llama_eval(
 
     const int d_key = n_embd/n_head;
 
-     // TODO: check if this size scales with n_ctx linearly and remove constant. somehow I feel it wasn't the case
+    // TODO: check if this size scales with n_ctx linearly and remove constant. somehow I feel it wasn't the case
     // static size_t buf_size = hparams.n_ctx*1024*1024;
     static size_t buf_size = 512u*1024*1024;
     static void * buf = malloc(buf_size);
@@ -593,8 +611,8 @@ bool llama_eval(
 
             // cur = attention_norm*cur
             cur = ggml_mul(ctx0,
-                        ggml_repeat(ctx0, model.layers[il].attention_norm, cur),
-                        cur);
+                           ggml_repeat(ctx0, model.layers[il].attention_norm, cur),
+                           cur);
         }
 
         // self-attention
@@ -615,22 +633,22 @@ bool llama_eval(
             // Q = Qcur.contiguous().view(n_embd/n_head, n_head, N).permute(0, 2, 1, 3)
             struct ggml_tensor * Q =
                 ggml_permute(ctx0,
-                        ggml_rope(ctx0,
-                            ggml_cpy(ctx0,
-                                Qcur,
-                                ggml_new_tensor_3d(ctx0, GGML_TYPE_F32, n_embd/n_head, n_head, N)),
-                            n_past, n_rot, 0),
-                        0, 2, 1, 3);
+                             ggml_rope(ctx0,
+                                       ggml_cpy(ctx0,
+                                                Qcur,
+                                                ggml_new_tensor_3d(ctx0, GGML_TYPE_F32, n_embd/n_head, n_head, N)),
+                                       n_past, n_rot, 0),
+                             0, 2, 1, 3);
 
             // K = Kmem.view(n_embd/n_head, n_head, n_past + N).permute(0, 2, 1, 3)
             struct ggml_tensor * K =
                 ggml_permute(ctx0,
-                        ggml_rope(ctx0,
-                            ggml_reshape_3d(ctx0,
-                                ggml_view_1d(ctx0, model.memory_k, (n_past + N)*n_embd, il*n_ctx*ggml_element_size(model.memory_k)*n_embd),
-                                n_embd/n_head, n_head, n_past + N),
-                            n_past, n_rot, 1),
-                        0, 2, 1, 3);
+                             ggml_rope(ctx0,
+                                       ggml_reshape_3d(ctx0,
+                                               ggml_view_1d(ctx0, model.memory_k, (n_past + N)*n_embd, il*n_ctx*ggml_element_size(model.memory_k)*n_embd),
+                                               n_embd/n_head, n_head, n_past + N),
+                                       n_past, n_rot, 1),
+                             0, 2, 1, 3);
 
             // K * Q
             struct ggml_tensor * KQ = ggml_mul_mat(ctx0, K, Q);
@@ -638,9 +656,9 @@ bool llama_eval(
             // KQ_scaled = KQ / sqrt(n_embd/n_head)
             struct ggml_tensor * KQ_scaled =
                 ggml_scale(ctx0,
-                        KQ,
-                        ggml_new_f32(ctx0, 1.0f/sqrt(float(n_embd)/n_head))
-                        );
+                           KQ,
+                           ggml_new_f32(ctx0, 1.0f/sqrt(float(n_embd)/n_head))
+                          );
 
             // KQ_masked = mask_past(KQ_scaled)
             struct ggml_tensor * KQ_masked = ggml_diag_mask_inf(ctx0, KQ_scaled, n_past);
@@ -651,10 +669,10 @@ bool llama_eval(
             // V_trans = Vmem.view(n_embd/n_head, n_head, n_past + N).permute(1, 2, 0, 3).contiguous()
             struct ggml_tensor * V_trans =
                 ggml_permute(ctx0,
-                        ggml_reshape_3d(ctx0,
-                            ggml_view_1d(ctx0, model.memory_v, (n_past + N)*n_embd, il*n_ctx*ggml_element_size(model.memory_v)*n_embd),
-                            n_embd/n_head, n_head, n_past + N),
-                        1, 2, 0, 3);
+                             ggml_reshape_3d(ctx0,
+                                             ggml_view_1d(ctx0, model.memory_v, (n_past + N)*n_embd, il*n_ctx*ggml_element_size(model.memory_v)*n_embd),
+                                             n_embd/n_head, n_head, n_past + N),
+                             1, 2, 0, 3);
 
             // KQV = transpose(V) * KQ_soft_max
             struct ggml_tensor * KQV = ggml_mul_mat(ctx0, V_trans, KQ_soft_max);
@@ -664,13 +682,13 @@ bool llama_eval(
 
             // cur = KQV_merged.contiguous().view(n_embd, N)
             cur = ggml_cpy(ctx0,
-                    KQV_merged,
-                    ggml_new_tensor_2d(ctx0, GGML_TYPE_F32, n_embd, N));
+                           KQV_merged,
+                           ggml_new_tensor_2d(ctx0, GGML_TYPE_F32, n_embd, N));
 
             // projection (no bias)
             cur = ggml_mul_mat(ctx0,
-                    model.layers[il].wo,
-                    cur);
+                               model.layers[il].wo,
+                               cur);
         }
 
         struct ggml_tensor * inpFF = ggml_add(ctx0, cur, inpSA);
@@ -683,18 +701,18 @@ bool llama_eval(
 
                 // cur = ffn_norm*cur
                 cur = ggml_mul(ctx0,
-                        ggml_repeat(ctx0, model.layers[il].ffn_norm, cur),
-                        cur);
+                               ggml_repeat(ctx0, model.layers[il].ffn_norm, cur),
+                               cur);
             }
 
             struct ggml_tensor * tmp = ggml_mul_mat(ctx0,
-                    model.layers[il].w3,
-                    cur);
+                                                    model.layers[il].w3,
+                                                    cur);
 
 
             cur = ggml_mul_mat(ctx0,
-                    model.layers[il].w1,
-                    cur);
+                               model.layers[il].w1,
+                               cur);
 
             // SILU activation
             cur = ggml_silu(ctx0, cur);
@@ -702,8 +720,8 @@ bool llama_eval(
             cur = ggml_mul(ctx0, cur, tmp);
 
             cur = ggml_mul_mat(ctx0,
-                    model.layers[il].w2,
-                    cur);
+                               model.layers[il].w2,
+                               cur);
         }
 
         cur  = ggml_add(ctx0, cur, inpFF);
@@ -718,8 +736,8 @@ bool llama_eval(
 
         // inpL = norm*inpL
         inpL = ggml_mul(ctx0,
-                    ggml_repeat(ctx0, model.norm, inpL),
-                    inpL);
+                        ggml_repeat(ctx0, model.norm, inpL),
+                        inpL);
     }
 
     // lm_head
@@ -876,6 +894,27 @@ void process_interactive_mode(gpt_vocab &vocab, gpt_params &params, std::vector<
     }
 }
 
+void set_interactive_mode_signal_handler() {
+#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
+    struct sigaction sigint_action;
+    sigint_action.sa_handler = sigint_handler;
+    sigemptyset(&sigint_action.sa_mask);
+    sigint_action.sa_flags = 0;
+    sigaction(SIGINT, &sigint_action, NULL);
+#elif defined (_WIN32)
+    signal(SIGINT, sigint_handler);
+#endif
+}
+
+void print_interactive_mode_info() {
+    fprintf(stderr, "== Running in chat mode. ==\n"
+#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__)) || defined (_WIN32)
+            " - Press Ctrl+C to interject at any time.\n"
+#endif
+            " - Press Return to return control to LLaMA.\n"
+            " - If you want to submit another line, end your input in '\\'.\n");
+}
+
 
 int main(int argc, char ** argv) {
     ggml_time_init();
@@ -895,7 +934,7 @@ int main(int argc, char ** argv) {
     // load the model
     {
         const int64_t t_start_us = ggml_time_us();
-        if (!llama_model_load(params.model, model, vocab, params.n_ctx)) {  
+        if (!llama_model_load(params.model, model, vocab, params.n_ctx)) {
             fprintf(stderr, "%s: failed to load model from '%s'\n", __func__, params.model.c_str());
             return 1;
         }
@@ -932,21 +971,7 @@ int main(int argc, char ** argv) {
 
 
     if (params.interactive) {
-#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__))
-        struct sigaction sigint_action;
-        sigint_action.sa_handler = sigint_handler;
-        sigemptyset (&sigint_action.sa_mask);
-        sigint_action.sa_flags = 0;
-        sigaction(SIGINT, &sigint_action, NULL);
-#elif defined (_WIN32)
-        signal(SIGINT, sigint_handler);
-		//Windows console ANSI color fix 
-		HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-		DWORD mode;
-		GetConsoleMode(hConsole, &mode);
-		SetConsoleMode(hConsole, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-#endif
-
+        set_interactive_mode_signal_handler();
         fprintf(stderr, "%s: interactive mode on.\n", __func__);
 
     }
@@ -965,12 +990,7 @@ int main(int argc, char ** argv) {
 
 
     if (params.interactive) {
-        fprintf(stderr, "== Running in chat mode. ==\n"
-#if defined (__unix__) || (defined (__APPLE__) && defined (__MACH__)) || defined (_WIN32)
-               " - Press Ctrl+C to interject at any time.\n"
-#endif
-               " - Press Return to return control to LLaMA.\n"
-               " - If you want to submit another line, end your input in '\\'.\n");
+        print_interactive_mode_info();
     }
 
     // we may want to slide the input window along with the context, but for now we restrict to the context length
@@ -987,8 +1007,6 @@ int main(int argc, char ** argv) {
     if (params.use_color) {
         printf(ANSI_COLOR_YELLOW);
     }
-
-    
 
     while (remaining_tokens > 0) {
         // predict
@@ -1028,7 +1046,7 @@ int main(int argc, char ** argv) {
             // some user input remains from prompt or interaction, forward it to processing
             while (embd_inp.size() > input_consumed) {
                 // fprintf(stderr, "%6d -> '%s'\n", embd_inp[input_consumed], vocab.id_to_token.at(embd_inp[input_consumed]).c_str());
- 
+
                 embd.push_back(embd_inp[input_consumed]);
                 last_n_tokens.erase(last_n_tokens.begin());
                 last_n_tokens.push_back(embd_inp[input_consumed]);
@@ -1055,7 +1073,7 @@ int main(int argc, char ** argv) {
         // in interactive mode, and not currently processing queued inputs;
         // check if we should prompt the user for more
         if (params.interactive && embd_inp.size() <= input_consumed) {
-          process_interactive_mode(vocab,params,embd_inp,input_consumed,remaining_tokens);
+            process_interactive_mode(vocab,params,embd_inp,input_consumed,remaining_tokens);
         }
 
         // end of text token
